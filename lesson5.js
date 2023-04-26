177cb184f673e37be0ba04461925824dc5e75454
Reggae Homer <mgthantsinmm21@gmail.com>
2023-04-26 12:29:31 +0630

    smallest multiple problem

---
+++ lesson5.js
@@ -0,0 +1,16 @@
+let num = 20;
+
+while(true){
+	let divisible = true;
+	for(let i=1; i<=20; i++){
+		if(num % i !== 0){
+			divisible = false;
+			break;
+		}
+	}
+	if(divisible){
+		console.log(num);
+		break;
+	}
+	num++;
+}
